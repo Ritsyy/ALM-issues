87a50c650768cf3c261f7fcc004976ba21a662e0
Ritsyy <richarupela95@gmail.com>
2016-06-07 17:48:59 +0530

    Accessed list through Board Id

--- trello-alm.go
+++ trello-alm.go
@@ -10,10 +10,10 @@ import(
 
 func main() {
 	// New Trello Client
-	var apikey, token, userName, boardName, listName string
+	var apikey, token, userName, boardId, listName string
 	flag.StringVar(&apikey, "apikey","", "Trello API key")
 	flag.StringVar(&token, "token","", "Trello Token")
-	flag.StringVar(&boardName, "boardName", "AtomicOpenShift Roadmap", "Search the board")
+	flag.StringVar(&boardId, "boardId", "nlLwlKoz", "Search the board")
 	flag.StringVar(&listName, "listName"	, "Epic Backlog","Search List from the specific Board")
 	flag.StringVar(&userName, "userName","", "your trello username")
 	flag.Parse()
@@ -21,7 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// User @trello
 	user, err := trello.Member(userName)
 	if err != nil {
@@ -30,30 +29,23 @@ func main() {
 	fmt.Println(user.FullName)
 
 	// @trello Boards
-	boards, err := user.Boards()
+	board, err := trello.Board(boardId)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(boards) > 0 {
-		for i:=0; i<len(boards);i++{
-			board := boards[i]
-			if strings.Compare(board.Name, boardName)==0{
-				fmt.Printf("* %v (%v)\n", board.Name, board.ShortUrl)
-				// @trello Board Lists
-				lists, err := board.Lists()
-				if err != nil {
-					log.Fatal(err)
-				}
-				for _, list := range lists {
-					if strings.Compare(list.Name, listName)==0{
-						fmt.Println("   - ", list.Name)
-						// @trello Board List Cards
-						cards, _ := list.Cards()
-						for _, card := range cards {
-							fmt.Println("      + ", card.Name)
-						}
-					}
-				}
+	fmt.Printf("* %v (%v)\n", board.Name, board.ShortUrl)
+	// @trello Board Lists
+	lists, err := board.Lists()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, list := range lists {
+		if strings.Compare(list.Name, listName)==0{
+			fmt.Println("   - ", list.Name)
+			// @trello Board List Cards
+			cards, _ := list.Cards()
+			for _, card := range cards {
+				fmt.Println("      + ", card.Name)
 			}
 		}
 	}
