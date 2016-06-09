32ad26e63051a26072c6fa821f88e35a7dcfde39
Ritsyy <richarupela95@gmail.com>
2016-06-09 14:26:20 +0530

    Minor fix

--- github-alm.go
+++ github-alm.go
@@ -22,14 +22,14 @@ func main() {
 	result, _, err := client.Search.Issues(query, opts)
 	totalCount, _ := json.Marshal(result.Total)
 	fmt.Println(string(totalCount))
-       issues := result.Issues
+        issues := result.Issues
 	for l, _ := range issues {
 		url, _ := json.Marshal(issues[l].URL)
 		title, _ := json.Marshal(issues[l].Title)
-               labels, _ := json.Marshal(issues[l].Labels[0].Name)
+                labels, _ := json.Marshal(issues[l].Labels[0].Name)
 		fmt.Println("issue url: ", string(url))
 		fmt.Println("title: ", string(title))
-               fmt.Println("labels: ", string(labels))
+                fmt.Println("labels: ", string(labels))
 	}
 
 	if err != nil {
